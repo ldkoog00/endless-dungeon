3a6e0bb799a2b9fcd03dde9c44b1bb3fe888bd77
Lampppp <ldkoog00@harrisonburg.k12.va.us>
2019-10-04 09:16:59 -0400

    some changes

--- main.lua
+++ main.lua
@@ -1,18 +1,75 @@
+local Map = require 'core/map'
+
+
 
 function love.load()
-  x = 400
-  y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  x1 = 400
+  y1 = 300
+  playerImg1 = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  x2 = 200
+  y2 = 575
+  playerImg2 = love.graphics.newImage('assets-1/player/base/octopode_2.png')
+  map = Map:new(13, 13)
+  wall = love.graphics.newImage('assets-1/dungeon/wall/cobalt_stone_12.png')
+  template = {
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall}, 
+    }
+  map = Map:new(template) 
 end
+
+
+
+
 
 
 function love.update(dt)
+  if love.keyboard.isDown('up') then
+    y1 = y1 - 5
+  end
+  if love.keyboard.isDown('down') then
+    y1 = y1 + 5
+  end
+  if love.keyboard.isDown('left') then
+    x1 = x1 - 5
+  end
+  if love.keyboard.isDown('right') then
+    x1 = x1 + 5
+  end
+
+  
+   if love.keyboard.isDown('w') then
+     y2 = y2 - 5
+   end
+   if love.keyboard.isDown('s') then
+     y2 = y2 + 5
+   end
+  
+   
+  if love.keyboard.isDown('a') then
+     x2 = x2 - 5
+   end
+   if love.keyboard.isDown('d') then
+    x2 = x2 + 5
+   end
+
 
 end
-
+  
 
 function love.draw()
-  love.graphics.print('Hello, world!', 0, 0)
-  love.graphics.draw(playerImg, x, y)
+  map:draw()
+  love.graphics.draw(playerImg1, x1, y1)
+  love.graphics.draw(playerImg2, x2, y2)
 end
-
