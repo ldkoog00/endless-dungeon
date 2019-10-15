cc612b6b2445a96524276b231e64bd4bc06a928f
Lampppp <ldkoog00@harrisonburg.k12.va.us>
2019-10-15 09:07:53 -0400

    khhhhgggfddsssssaqqqqqwertyuikkkkkkkkkjnhhhhhhhgyyyyyytgfvbbbbbbbnm,,,,,,,.........,l,mmmmmjubiyuvuutcr56exer64zwxedcfvbnjlk;;;;;;;;;;;[0-olo,,ooooooooinuibyyv owo

--- main.lua
+++ main.lua
@@ -6,19 +6,26 @@ local Util = require 'core/util'
 function love.load()
   x = 100
   y = 100
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
   w = 64   
-  h = 64   
-  hp = 100 
+  h = 55   
+  hp = 100   
+  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  
+  xS = 100
+  yS = 100
+  wS = 20
+  hS = 30
+  enemyImg = love.graphics.newImage('assets-2/UNUSED/monsters/skeleton_small.png')
   
   map = Map:new(10, 13)
   map = Map:new(10, 13)
   floor = love.graphics.newImage('assets-1/dungeon/floor/crypt_domino_8a.png')
   wall  = love.graphics.newImage('assets-1/dungeon/wall/lab-metal_0.png')
+  door  = love.graphics.newImage('assets-1/dungeon/doors/runed_door.png')
 
   walls = {
     { wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
-    { wall, nil, nil, nil, nil, nil, nil, nil, nil, wall},
+    { door, nil, nil, nil, nil, nil, nil, nil, nil, wall},
     { wall, nil, nil, nil, nil, nil, nil, nil, nil, wall},
     { wall, nil, nil, nil, nil, nil, nil, nil, nil, wall},
     { wall, nil, nil, nil, nil, nil, nil, nil, nil, wall},
@@ -72,17 +79,38 @@ function love.update(dt)
      end
   end
   if love.keyboard.isDown('right') then
-    if map:cc(x + 3, y, w, h) == false then  
+    if map:cc(x + 5, y, w, h) == false then  
       x = x + 5
     end
+  end
+    
+  
+  if love.keyboard.isDown('w') then
+      if map:cc(xS, yS - 5, wS, hS) == false then  
+        yS = yS - 5
+       end
+    end
+    if love.keyboard.isDown('s') then
+      if map:cc(xS, yS + 5, wS, hS) == false then  
+        yS = yS + 5
+       end
+    end
+    if love.keyboard.isDown('a') then
+      if map:cc(xS - 5, yS, wS, hS) == false then  
+        xS = xS - 5
+       end
+    end
+    if love.keyboard.isDown('d') then
+      if map:cc(xS + 5, yS, wS, hS) == false then  
+        xS = xS + 5
+      end
     
   end
-  
 
  
- if map:cc(x, y, w, h) then  
-  hp = hp - 1
- end
+  if map:cc(x, y, w, h) then  
+   hp = hp - 1
+  end
 end
   
 
@@ -95,11 +123,7 @@ function love.draw()
   map:draw()
   
   love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(enemyImg, xS, yS)
    
-  love.graphics.rectangle('line', 0, 0, 832, 64)
-  love.graphics.rectangle('line', 0, 0, 64, 640)
-  love.graphics.rectangle('line', 0, 576, 832, 64)
-  love.graphics.rectangle('line', 768, 0, 64, 640)
-  love.graphics.rectangle('line', 384, 256, 128, 128)
    love.graphics.print(hp, 0, 0)
 end
